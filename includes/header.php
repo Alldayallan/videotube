f1f53eafeff04ecc1ae75dd0c9cac82ddb54cf75
Alldayallan <mail@allanreid.co>
2022-01-31 13:22:11 -0800

    first commit

---
+++ includes/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VideoTube</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
+</head>
+<body>
+
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png" title="menu" alt="menu">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="site logo">
+            </a>
+
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                    <img src="assets/images/icons/search.png" title="search" alt="search">
+                    </button>
+                </form>
+            </div>
+
+            <div class="rightIcons">
+                <a href="upload.php">
+                    <img class="upload" src="assets/images/icons/upload.png" title="upload" alt="upload">
+                </a>
+                <a href="#">
+                    <img class="upload" src="assets/images/profilePictures/default.png" title="profile picture" alt="default profile picture">
+                </a>
+            </div>
+
+        </div>
+
+        <div id="sideNavContainer" style="display:none;">
+
+        </div>
+
+        <div id="mainSectionContainer">
+
+        <div id="mainContentContainer">
